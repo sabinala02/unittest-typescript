d3d40a9c2dacc2f41908e824c9cad4a3199a33eb
Sabina Lazri <sabina_lazri1@sluz.ch>
2026-03-13 14:56:47 +0100

    Aufgabe 5.2 add new test with five numbers

--- src/average.test.ts
+++ src/average.test.ts
@@ -11,3 +11,15 @@ test("test mean of two numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [2, 6, 4, 8, 10];
+  const expected: number = 6;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
