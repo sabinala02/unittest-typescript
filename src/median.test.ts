c89e2bf98b9b61d8bd9bbfbbabd06288f9ff5100
Sabina Lazri <sabina_lazri1@sluz.ch>
2026-03-13 15:15:37 +0100

    Aufgabe 6.2 implement median

--- src/median.test.ts
+++ src/median.test.ts
@@ -3,7 +3,7 @@ import { median } from "./median";
 test("test median for odd number of elements", () => {
     // Arrange
     const numbers: number[] = [2, 6, 4];
-    const expected: number = 6;
+    const expected: number = 4;
 
     // Act
     const actual: number = median(numbers);
--- src/median.ts
+++ src/median.ts
@@ -1,3 +1,11 @@
 export function median(numbers: number[]): number {
-    return 0.0;
+    if (numbers.length === 0) {
+        throw new Error("Cannot calculate the median of an empty array");
+    }
+    const sortedNumbers: number[] = [...numbers].sort((a, b) => a - b);
+    const middleIndex: number = Math.floor(sortedNumbers.length / 2);
+    if (sortedNumbers.length % 2 === 0) {
+        return (sortedNumbers[middleIndex - 1] + sortedNumbers[middleIndex]) / 2;
+    }
+    return sortedNumbers[middleIndex];
 }
